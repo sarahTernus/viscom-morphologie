e7024c97299469932c73c89a3bb43d7ad49662db
sarahTernus <s.ternus@web.de>
2022-07-22 20:36:38 +0200

    added script for salt and pepper noise removal via opening and closing

---
+++ remove_noise.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2 as cv
+import random
+
+
+def add_noise(image):
+    row, col = image.shape
+
+    number_of_pixels = random.randint(300, 10000)
+    for i in range(number_of_pixels):
+        y_coord = random.randint(0, row - 1)
+
+        x_coord = random.randint(0, col - 1)
+
+        image[y_coord][x_coord] = 255
+
+    number_of_pixels = random.randint(300, 10000)
+    for i in range(number_of_pixels):
+        y_coord = random.randint(0, row - 1)
+
+        x_coord = random.randint(0, col - 1)
+
+        image[y_coord][x_coord] = 0
+
+    return image
+
+
+def salt_pepper_noise_removal(image):
+
+    kernel = np.ones((2, 2), np.uint8)
+    print(kernel)
+    print(image)
+
+    opening = cv.morphologyEx(image, cv.MORPH_OPEN, kernel)
+    closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
+
+    return closing
+
+
+if __name__ == '__main__':
+    img = cv.imread('images/erau.jpeg', cv.IMREAD_GRAYSCALE)
+    salt_and_pepper_erau = add_noise(img)
+    cv.imwrite('images/salt-pepper-erau.jpg', salt_and_pepper_erau)
+
+    cv.imwrite('images/removed-noise-erau.jpg', salt_pepper_noise_removal(salt_and_pepper_erau))
